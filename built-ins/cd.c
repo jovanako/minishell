583124fb2e5c092d1efcd2792ab70e4c8f1e38ae
Jovana <jovana1005@gmail.com>
2025-07-17 20:45:04 +0200

    fixed ft_cd OLDPWD bug

--- built-ins/cd.c
+++ built-ins/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: culbrich <culbrich@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jkovacev <jkovacev@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 12:55:18 by culbrich          #+#    #+#             */
-/*   Updated: 2025/06/27 14:41:11 by culbrich         ###   ########.fr       */
+/*   Updated: 2025/07/17 20:36:01 by jkovacev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_cd(char *path, t_list *lst)
 	if (chdir(path))
 		return (1);
 	// will need protection against invalid input
-	ft_modenv(ft_getenv(lst, "OLDPWD"), "PWD");
+	ft_modenv(ft_getenv(lst, "OLDPWD"), ft_getenv_v(ft_getenv(lst, "PWD")));
 	ft_modenv(ft_getenv(lst, "PWD"), path);
 	return (0);
 }
---
+++ tests.sh
@@ -0,0 +1,44 @@
+test_output_contains() {
+    local expected="$1"
+    local input_command="$2"
+
+    # Run ./minishell with heredoc input and capture output
+    local output
+    output=$(./minishell <<EOF
+$input_command
+EOF
+)
+
+	# Define ANSI color codes
+    GREEN='\033[0;32m'
+    RED='\033[0;31m'
+    NC='\033[0m' # No Color / reset
+
+    # Check if the expected string is in the output
+    if grep -q "$expected" <<< "$output"; then
+        echo -e "${GREEN}PASS:${NC} '$expected' found in output of '$input_command'"
+    else
+        echo -e "${RED}FAIL:${NC} '$expected' not found in output of '$input_command'"
+        echo "Output was:"
+        echo "$output"
+    fi
+}
+
+test_output_contains "hello" "echo hello"
+test_output_contains "bla" "echo bla | grep a"
+test_output_contains "oranges" \
+'echo hello > infile
+echo world >> infile
+echo apples >> infile
+echo oranges >> infile
+cat infile | sort | grep g'
+test_output_contains "hello" \
+'echo hello > infile
+cat infile'
+test_output_contains "$USER" 'echo $USER'
+test_output_contains "USER=$USER" "env | grep USER="
+test_output_contains "XXX=$XXX" "XXX=xxx env | grep XXX"
+test_output_contains "/home/jovana/Desktop/42Berlin/minishell" "pwd"
+# test_output_contains "/home/jovana/Desktop/42Berlin" \
+# 'cd ..
+# pwd'
