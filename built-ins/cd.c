34def2fee926da5707e9f49191b581dcd99562f7
Jovana <jovana1005@gmail.com>
2025-07-27 20:55:57 +0200

    added error handling in cd for too many arguments

--- built-ins/cd.c
+++ built-ins/cd.c
@@ -6,7 +6,7 @@
 /*   By: jkovacev <jkovacev@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 12:55:18 by culbrich          #+#    #+#             */
-/*   Updated: 2025/07/27 20:37:52 by jkovacev         ###   ########.fr       */
+/*   Updated: 2025/07/27 20:53:49 by jkovacev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@
 // changes cwd to path and updates env vars accordingly
 int	ft_cd(char **argv, t_list *lst)
 {
+	if (array_size(argv) > 2)
+	{
+		printf("minishell: cd: too many arguments\n");
+		return (1);
+	}
 	if (!argv[1])
 		argv[1] = ft_getenv_v(ft_getenv(lst, "HOME"));
 	if (chdir(argv[1]))
